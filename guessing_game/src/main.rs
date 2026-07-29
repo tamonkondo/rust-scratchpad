aab9ca827efdca79087ffd5b201868a55a84e426
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-07-29 22:48:41 +0900

    Enum定義読破
    https://doc.rust-jp.rs/book-ja/ch06-01-defining-an-enum.html

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,53 +1,34 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn width(&self) -> bool {
-        self.width > 0
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    fn create(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
-    fn union(rectangles: &[Rectangle]) -> u32 {
-        rectangles.iter().map(|r| r.width * r.height).sum()
-    }
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// fn route(ip_kind: IpAddrKind) {}
+enum Message {
+    Ouit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+enum Option<T> {
+    None,
+    Some(T),
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-    // インスタンスで呼び出すことも可能
-    let square = Rectangle::create(2);
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
 
-    println!("rect1 area is {}", rect1.area());
-    if rect1.width() {
-        println!("rect1のwidthは{}です。", rect1.width);
-    } else {
-        println!("rect1のwidthは無く、{}です。", rect1.width);
-    }
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!("Can rect1 hold square? {}", rect1.can_hold(&square));
-    println!("total area {}", Rectangle::union(&[rect1, rect2]))
+    let home = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let loopback = IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1));
+    let some_number = Option::Some(5);
+    let some_char = Option::Some('e');
+
+    let absent_number: Option<i32> = Option::None;
 }
