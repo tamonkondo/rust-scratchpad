4249c57cda33eb467ed1149dab900dcf246ccb87
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-08-19 23:22:25 +0900

    エラー読破 https://doc.rust-jp.rs/book-ja/ch09-03-to-panic-or-not-to-panic.html

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,28 +1,52 @@
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Error, Read};
 
+fn read_username_from_file() -> Result<String, Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
 fn main() {
-    // let mut scores = HashMap::new();
-    // scores.insert("Blue", 10);
-    // scores.insert("Yellow", 50);
+    // 行数が呼ばれるのでエラー元が分かる。
+    // panic!("crash and burn")
+    // let v = vec![1,2,3];
 
-    // let team_name = "Blue";
-    // let score = scores.get(&team_name).copied().unwrap_or(0);
+    // v[99];
+    // let error_message = greeting_file_result
+    //     .as_ref()
+    //     .err()
+    //     .map(|e| e.to_string())
+    //     .unwrap_or_default();
 
-    // println!("{}",score);
-    // let mut a =  [1, 2];
-    // let b = &mut a; // ここでaは借りられているので、a自体を変更することはできない。
-    // // a[0] = 1;
-    // b[0] = 1;
-    // println!("{:p}", &b);
-    // let mut a: [i32; 2] = [1, 2];
-    // a[0] = 6;
-    // let mut b = a;
-    // b[0] = b[0] + 4;
-    // // これは同じ
-    // println!("{:?}", &a);
-    // println!("{:?}", &b);
+    // let greeting_file_result = File::open("hello.txt");
+    // エラーの内容によって条件分岐ができる。
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("ファイルを作成するのに問題がありました。{:?}", e),
+    //         },
+    //         other_error => {
+    //             //     "ファイルを開くのに問題がありました: {:?}"
+    //             panic!("Problem opening the file: {:?}", other_error);
+    //         }
+    //     },
+    // };
+    // matchの省略版
+    // let greeting_file = greeting_file_result.unwrap();
+    // let greeting_file = greeting_file_result.expect("hello.txtは含まれるべき");
 
-    let mut a = 1;
-    a = 2;
-    println!("{:?}", a);
+    let result = read_username_from_file();
+    println!("{:?}", result);
 }
